5496b19080b6d8cc86771acd6efd640090bbe734
Aleksandr.Khvostov <aleksanfr.khvostov@glowbyteconsulting.com>
2020-07-28 12:22:35 +0300

    6.0.2. Заполнение массива. [#332243]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         boolean[] flags = new boolean[1000];
+        String[] names = new String[5];
+        names[0] = "Вася";
+        names[1] = "Коля";
+        names[2] = "Петя";
+        names[3] = "Вова";
+        names[4] = "Саша";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
