a5ba25ea37cd321b40b0ffa0c9dc4885c157921d
Aleksandr.Khvostov <aleksanfr.khvostov@glowbyteconsulting.com>
2020-08-14 22:17:08 +0300

    3. Интерфейс Input [#332366]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,21 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    int passengers = 0;
+
+    @Override
+    public void drive() {
+        System.out.println("Drive Drive Drive");
+    }
+
+    @Override
+    public void passengers(int count) {
+        passengers += count;
+    }
+
+    @Override
+    public double tank(double liters) {
+        return liters * 3.14;
+    }
+}
