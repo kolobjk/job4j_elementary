3b20d2c55bad4ab6d09b0f89de0142f249a897fd
Aleksandr.Khvostov <aleksanfr.khvostov@glowbyteconsulting.com>
2020-08-20 22:08:09 +0300

    6.4. Слово начинается с ... [#332231]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -4,15 +4,11 @@ public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        if (pref.length < word.length) {
-            for (int i = 0; i < pref.length; i++) {
-                if (word[i] != pref[i]) {
-                    result = false;
-                    break;
-                }
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]) {
+                result = false;
+                break;
             }
-        } else {
-            result = false;
         }
         return result;
     }
