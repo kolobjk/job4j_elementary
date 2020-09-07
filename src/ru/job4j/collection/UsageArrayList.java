0af1f78c7392b728934a34a0a63ee277ca8b8702
Aleksandr.Khvostov <aleksanfr.khvostov@glowbyteconsulting.com>
2020-09-07 22:30:48 +0300

    0. Коллекции, ArrayList, List, Обобщения [#332349]

---
+++ src/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> stringArrayList = new ArrayList<String>();
+        stringArrayList.add("Petr");
+        stringArrayList.add("Ivan");
+        stringArrayList.add("Stepan");
+        for (String element: stringArrayList) {
+            System.out.println(element);
+        }
+    }
+}
